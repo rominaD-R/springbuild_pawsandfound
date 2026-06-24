11c18aa5d030964684b104141fed9de97903a370
Romina Diaz-Rivero <romina.diazrivero@gmail.com>
2026-06-24 18:58:59 -0500

    Spring build repo

---
+++ src/main/java/org/example/springbuild_pawsandfound/SpringbuildPawsandfoundApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbuild_pawsandfound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbuildPawsandfoundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbuildPawsandfoundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbuild_pawsandfound/SpringbuildPawsandfoundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbuild_pawsandfound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbuildPawsandfoundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
